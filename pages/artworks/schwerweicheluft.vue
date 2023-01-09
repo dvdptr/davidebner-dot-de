e69c107885de7a89789334bef51ced5e449a42f3
dvdptr <info@davidebner.de>
2023-01-09 09:54:32 +0100

    reduce image size

--- pages/artworks/schwerweicheluft.vue
+++ pages/artworks/schwerweicheluft.vue
@@ -7,7 +7,7 @@
       material="Blei, Luftkissen, Koffer / 250x250x100 cm"
       description="Die Installation »Schwer weiche Luft« besteht aus 100 Blei-Splittern, die auf Luftkissen liegen. Die gegensätzlichen Materialien von leichten, weichen Luftkissen und schweren, harten Bleistücken werden hierbei in Verbindung zueinander gesetzt. Aus einem im Raum platzierten Koffer scheinen sich die Luftkissen und Bleisplitter auszubreiten. Die Form erweckt den Eindruck einer fließenden Bewegung. Dies wird durch den Koffer als Objekt der Mobilität verstärkt. Die Schwere des Bleis wirkt jedoch gegensätzlich und drückt die Luftkissen zu Boden und unterbindet Bewegung.
 Die Splitter wurden in einem Bleiguss-Verfahren erstellt: Insgesamt 4 individuell gebrochene Stücke aus Holz wurden zu einer Gesamtzahl von 100 Blei-Splitter dupliziert. So sind bei genauer Betrachtung der Stücke feinste Holzfasern und Bruchstellen erkennbar, die in einem hellsilbrigen Glanz scheinen."
-      :images="[{ src: swl1 }]"
+      :images="[{ src: swl1 }, { src: swl2 }, { src: swl3 }]"
     />
   </div>
 </template>
@@ -15,7 +15,9 @@ Die Splitter wurden in einem Bleiguss-Verfahren erstellt: Insgesamt 4 individuel
 <script>
 import WorksDetail from "~/components/WorksDetail.vue";
 
-import swl1 from "../../assets/exhibitions/zadyma.jpg";
+import swl1 from "../../assets/artworks/za1.jpg";
+import swl2 from "../../assets/artworks/za3.jpg";
+import swl3 from "../../assets/artworks/za4.jpg";
 
 export default {
   components: {
@@ -24,6 +26,8 @@ export default {
   data() {
     return {
       swl1,
+      swl2,
+      swl3,
     };
   },
 };
